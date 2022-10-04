50f1e5c1424a49de3adde2d55359da16e19ed4a1
mabrar02 <mahdeen02@gmail.com>
2022-10-04 00:02:03 -0300

    CubeGame
    Simple 3D game where you dodge obstacles as a cube and try to make it to the end. Followed this tutorial for the most part: https://www.youtube.com/watch?v=IlKaB1etrik

---
+++ Cube Game/Cube Game/Assets/Scripts/Score.cs
@@ -0,0 +1,15 @@
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public TextMeshProUGUI score;
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = player.position.z.ToString("0");
+    }
+}
